43986ccac5e7dd14857a6e9fbe9536c219cee10a
ahmadm2001 <mashalm2001@gmail.com>
2022-12-13 18:54:39 +0200

    Initial commit

---
+++ app/src/main/java/com/example/alfaversion/StudentLogIN.java
@@ -0,0 +1,14 @@
+package com.example.alfaversion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentLogIN extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_log_in);
+    }
+}
